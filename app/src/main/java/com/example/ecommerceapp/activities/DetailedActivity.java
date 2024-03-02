6332aba5725aa218a39e3899a0a72e14dd8586ec
sbusi <sbusiso@gmail.com>
2024-03-02 20:11:04 +0200

    6th Commit

--- app/src/main/java/com/example/ecommerceapp/activities/DetailedActivity.java
+++ app/src/main/java/com/example/ecommerceapp/activities/DetailedActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -56,6 +57,13 @@ public class DetailedActivity extends AppCompatActivity {
         toolbar = findViewById(R.id.detailed_toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
         fireStore = FirebaseFirestore.getInstance();
 
@@ -118,6 +126,27 @@ public class DetailedActivity extends AppCompatActivity {
             totalPrice = showAllModel.getPrice() * totalQuantity;
         }
 
+        //Buy Now
+        buyNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DetailedActivity.this,AddressActivity.class);
+
+                if(newProductsModel != null){
+                    intent.putExtra("item", newProductsModel);
+                }
+                if(popularProductsModel != null){
+                    intent.putExtra("item",popularProductsModel);
+                }
+                if(showAllModel != null){
+                    intent.putExtra("item",showAllModel);
+                }
+                startActivity(intent);
+            }
+        });
+
+
+        //Add To cart
         addToCart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
